83852dbf5f5f958bdd56508783e2d29631f6734a
Anthony <elmorodjeur@gmail.com>
2018-07-18 23:13:00 +0200

    first commit, install react and started to install redux

---
+++ src/actions/tshirtActions.js
@@ -0,0 +1,6 @@
+export const DELETE_ARTICLE = "DELETE_ARTICLE"
+
+export const deleteArticle = () => ({
+  type: DELETE_ARTICLE
+})
+
---
+++ src/reducers/TshirtReducer.js
@@ -0,0 +1,23 @@
+// const initialState = []
+
+// const Tshirtreducer = [
+ 
+//   {
+//   taille : "xl",
+//   color : "blue" ,
+// },
+// {
+//   taille : "l",
+//   color : "red" ,
+// }]
+
+// const Tshirtreducer = (prevstate=initialState,action) => {
+//   return prevstate
+// }
+
+// export default Tshirtreducer
+
+
+const Tshirtreducer = () => null ;
+
+export default Tshirtreducer;
